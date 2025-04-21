ca9e7bad17c3c6121b406047a7882fd2e51f9127
Kunal <kunalshinde090205@gmail.com>
2025-04-21 11:23:54 +0530

    Sign in and Sign up feature added

--- oops_project.py
+++ oops_project.py
@@ -6,12 +6,12 @@ class Chatbook:
         self.Menu()
 
     def Menu(self):
-        user_input = input("""Welcome to Chatbook! Please choose an option:\n1. SignUp\n2. SignIn\n3. Post\n \4.Message a friend \n5. Exit\n""")
+        user_input = input("""Welcome to Chatbook! Please choose an option:\n1. SignUp\n2. SignIn\n3. Post\n4.Message a friend \n5. Exit\n""")
 
         if user_input == "1":
-            pass
+            self.SignUp()
         elif user_input == "2":
-            pass
+            self.SignIn()
         elif user_input == "3":
             pass
         elif user_input == "4":
@@ -23,5 +23,28 @@ class Chatbook:
             print("Invalid input. Please try again.")
             self.Menu()
 
+
+    def SignUp(self):
+        user = input("Please enter your username: ")
+        pwd = input("Please enter your password: ")
+        self.username = user
+        self.password = pwd
+        print("Account created successfully!")
+        self.Menu()
+
+    def SignIn(self):
+        if self.username == "" or self.password == "":
+            print("Please create an account first.")
+            self.Menu()
+        else:
+            username = input("Please enter your username: ")
+            password = input("Please enter your password: ")
+            if username == self.username and password == self.password:
+                print("Login successful!")
+                self.LoggedIn = True
+                self.Menu()
+            else:
+                print("Invalid credentials. Please try again.")
+                self.SignIn()
 
 Kunal = Chatbook()
